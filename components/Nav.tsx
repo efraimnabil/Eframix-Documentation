708139a1a6e0cdb77029a293afef0f78d0faa7d5
Mina <m.magdy@paywithspare.com>
2024-10-30 14:17:35 +0300

    feat(nav): add Eframix logo in navigation bar
    - Added a rounded Eframix logo to the navigation bar for improved branding.
    - Included new logo assets: `eframix-logo-transformed.jpeg`, `eframix-logo-transparent.png`, and `logo.png` in the `public` directory.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -21,6 +21,7 @@ export default function Nav() {
     <header className="sticky top-0 z-50 w-full px-3 md:px-6 lg:px-10 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
         <Link href="/" className="mr-6 flex items-center space-x-2">
+          <img src="/logo.png" alt="logo" className="rounded-full w-9" />
           <span className="font-bold">Eframix</span>
         </Link>
         <div className="mr-4 hidden md:flex">
